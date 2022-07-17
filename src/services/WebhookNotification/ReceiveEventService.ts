f4754900633e0998106f11ec28b73b514e3376f1
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2022-07-17 17:28:34 -0300

    Saving message received from webhook

--- src/services/WebhookNotification/ReceiveEventService.ts
+++ src/services/WebhookNotification/ReceiveEventService.ts
@@ -1,3 +1,6 @@
+import { randomUUID } from "crypto";
+import CreateMessageService from "../MessageServices/CreateMessageService";
+
 /* eslint-disable camelcase */
 type WebhookNotification = {
   object: string;
@@ -44,7 +47,22 @@ export const ReceiveEventService = async (
   body: WebhookNotification
 ): Promise<unknown> => {
   console.log(body.entry[0].changes[0].value.messages[0]);
-  return body.entry[0].changes[0].value.messages[0];
+
+  const messageReceived = body.entry[0].changes[0].value.messages[0];
+
+  const messageData = {
+    id: randomUUID(),
+    ack: 1,
+    ticketId: 1,
+    body: messageReceived.text.body,
+    fromMe: false,
+    read: true,
+    mediaType: messageReceived.type === "text" ? "chat" : messageReceived.type,
+    quotedMsgId: null
+  };
+
+  // await ticket.update({ lastMessage: body });
+  return CreateMessageService({ messageData });
 };
 
 export default ReceiveEventService;
