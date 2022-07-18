43aa5c2d5e241f2c0d590c37a12e2ce718546804
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2022-07-18 07:14:45 -0300

    Ajuste na tipagem do recebimento do webhook

--- src/services/WebhookNotification/ReceiveEventService.ts
+++ src/services/WebhookNotification/ReceiveEventService.ts
@@ -5,75 +5,37 @@ import CreateMessageService from "../MessageServices/CreateMessageService";
 
 /* eslint-disable camelcase */
 type WebhookNotification = {
-  object: string;
-  entry: [
+  contacts: [
     {
-      id: number;
-      time: number;
-      changes: [
-        {
-          value: {
-            messaging_product: string;
-            metadata: {
-              display_phone_number: number;
-              phone_number_id: number;
-            };
-            contacts: [
-              {
-                profile: {
-                  name: string;
-                };
-                wa_id: number;
-              }
-            ];
-            messages: [
-              {
-                from: number;
-                id: string;
-                timestamp: Date;
-                text?: {
-                  body: string;
-                };
-                type: string;
-                button?: {
-                  payload: string;
-                  text: string;
-                };
-              }
-            ];
-          };
-          field: string;
-        }
-      ];
+      profile: {
+        name: string;
+      };
+      wa_id: number;
+    }
+  ];
+  messages: [
+    {
+      from: number;
+      id: string;
+      timestamp: Date;
+      text?: {
+        body: string;
+      };
+      type: string;
+      button?: {
+        payload: string;
+        text: string;
+      };
     }
   ];
 };
 
-// interface WebhookNotification {
-//   context: {
-//     from: string;
-//     id: string;
-//   };
-//   from: string;
-//   id: string;
-//   timestamp: string;
-//   type: string;
-//   text?: {
-//     body: string;
-//   };
-//   button?: {
-//     payload: string;
-//     text: string;
-//   };
-// }
-
 export const ReceiveEventService = async (
   body: WebhookNotification
 ): Promise<unknown> => {
-  console.log(body.entry[0].changes[0].value.messages[0]);
+  console.log(body.messages);
 
-  const { from, type, text, button } =
-    body.entry[0].changes[0].value.messages[0];
+  const { from, type, text, button } = body.messages[0];
 
   const contact = await Contact.findOne({
     where: { number: from }
